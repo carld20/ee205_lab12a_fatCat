b1cea484244c84c79ec238af4ded4654363e1d66
Carl Domingo <calrld20@hawaii.edu>
2022-04-13 09:55:41 -1000

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Carl Domingo <carld20@hawaii.edu>
+/// @date   13_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
